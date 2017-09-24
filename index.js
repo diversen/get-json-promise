2f972446fa823037b260ad2ec6683a273f2d43f4
Dennis Iversen <dennis.iversen@gmail.com>
2017-09-24 13:09:47 +0200

    add files

---
+++ index.js
@@ -0,0 +1,21 @@
+function getJSONPromise(url) {
+
+    var promise = new Promise((resolve, reject) => {
+        var request = new XMLHttpRequest();
+
+        request.open('get', url, true);
+        request.responseType = 'text';
+        request.onload = function () {
+            if (request.status === 200) {
+                resolve(JSON.parse(request.responseText));
+            } else {
+                reject('JSON could not be loaded ' + url);
+            }
+        };
+        request.send();
+    });
+
+    return promise;
+}
+
+module.exports = getJSONPromise;
