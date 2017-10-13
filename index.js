40b9aae718f7858879fbef96630ac3750e948434
Dennis Iversen <dennis.iversen@gmail.com>
2017-10-13 22:45:16 +0200

    throw

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ function getJSONPromise(url) {
         request.responseType = 'text';
         request.onload = function () {
             if (request.status === 200) {
-                try {
-                    JSON.parse(request.responseText);
-                } catch (error) {
-                    reject('JSON could not be parsed. File ' + url +  'Error ' + error);
-                }
-
                 resolve(JSON.parse(request.responseText));
             } else {
                 reject('JSON could not be loaded ' + url);
